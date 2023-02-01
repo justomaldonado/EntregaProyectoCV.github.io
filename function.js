fffd7bc330c560cb1af4e1f8e989a5f25e20a468
justomaldonado <123600948+justomaldonado@users.noreply.github.com>
2023-02-01 10:44:24 -0300

    Add files via upload

---
+++ function.js
@@ -0,0 +1,6 @@
+
+document.getElementById('boton').onclick = function (){
+    console.log("Capturamos el evento click");
+    document.getElementById('demo').innerHTML = "25 de Enero del año 2023"
+}
+    
